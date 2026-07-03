52d1cb84051d9e4c7949f3fa508372422dac9937
Janushiya <janushiyamurugesan0218@gmail.com>
2026-07-03 22:08:13 +0530

    Add files via upload

---
+++ Main (10).java
@@ -0,0 +1,11 @@
+
+
+public class Main
+{
+	public static void main(String[] args) {
+       int Fee=700;
+       int Team=12;
+       int TotalFee=Fee*Team;
+		System.out.println("Total Fee="+TotalFee);
+	}
+}
